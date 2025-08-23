2336deca8ebd51b054b634b35c6fe8aaa04f2612
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-08-23 10:43:05 -0700

    album added

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
+
+import { albumsData } from '../assets/assets'
+import AlbumItem from './Albumitem' 
 function DisplayHome() {
   return (
     <>
       <Navbar/>
+      <div className='my-5 font-bold text-2xl'>
+        {albumsData.map((item,index)=>(<AlbumItem key={index} image={item.image} name={item.name} desc={item.desc} id={item.id}/>))}
+      </div>
+      
     </>
   )
 }
