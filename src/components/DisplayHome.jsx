f8edfe194eb28feb945c9f5780365bf14b212e42
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-08-26 11:43:41 -0700

    song item is added

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import Navbar from './Navbar'
 import { albumsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
+import { songsData } from '../assets/assets'
 
 function DisplayHome() {
   return (
     <>
       <Navbar/>
-      <div className='my-5 font-bold text-2xl'>
-        {albumsData.map ((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id }image={item.image}/>))}
+      <div className='mb-4'>
+        <h1 className='text-2xl font-bold my-5'>Featured charts</h1>
+        <div className='flex overflow-auto'>
+          {albumsData.map ((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id }image={item.image}/>))}
+        </div>
+      </div>
+      <div className='mb-4'>
+        <h1 className='text-2xl font-bold my-5'>Todays biggest hit</h1>
+        <div className='flex overflow-auto'>
+          {}
+        </div>
       </div>
     </>
   )
