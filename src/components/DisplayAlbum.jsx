8b17c1ba971ec40a4c8553aacedcbda8909a82cb
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-09-01 13:43:10 -0700

    display album data

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import { useParams } from 'react-router-dom'
+import { albumsData } from '../assets/assets';
 const DisplayAlbum = () => {
+
+    const {id}= useParams();
+    const albumData = albumsData[id];
+    
   return (
     <>
       <Navbar/>
