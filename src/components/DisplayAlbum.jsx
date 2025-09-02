e9e91c8c5f707f64181ce1164dff497cd690e8d6
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-09-02 02:36:02 -0700

    navigation functionality is added

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData, assets } from '../assets/assets';
+import { albumsData, assets, songsData } from '../assets/assets';
 const DisplayAlbum = () => {
 
     const {id}= useParams();
@@ -18,13 +18,34 @@ const DisplayAlbum = () => {
                  <h4>{albumData.desc}</h4>
                  <p className='mt-1'>
                     <img className='w-5 inline-block mr-1' src={assets.spotify_logo}/>
-                    <b>Spotify</b>
+                    <b>Spotify </b>
                     .1323154 likes
-                    .<b>50 songs</b>
+                    .<b> 50 songs </b>
                      about 3 hr 37 min
                  </p>
             </div>
         </div>
+        <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
+           <p><b className='mr-4'>#</b>Title</p>
+           <p>Album</p>
+           <p className='hidden sm:block'>Date Added</p>
+           <img className='m-auto w-4'src={assets.clock_icon}/>
+        </div>
+        <hr/>{
+            songsData.map((item,index)=>(
+                <div key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff26] cursor-pointer'>
+                     <p className='text-white'>
+                        <b className='mr-3 text-[#a7a7a7]'>{index+1}</b>
+                        <img className='inline w-10 mr-5' src={item.image} alt=""/>
+                        {item.name}
+                     </p>
+                     <p className='text-[15px]'>{albumData.name}</p>
+                     <p className='text-[15px] hidden sm:block'>5-days ago</p>
+                     <p className='text-[15px] text-center'>{item.duration}</p>
+
+                </div>
+            ))
+        }
     </>
   )
 }
