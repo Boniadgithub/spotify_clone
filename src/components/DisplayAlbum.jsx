be76940f51bebc45af1fa8801f183746bcef980c
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-09-02 00:54:54 -0700

    album info added

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData } from '../assets/assets';
+import { albumsData, assets } from '../assets/assets';
 const DisplayAlbum = () => {
 
     const {id}= useParams();
@@ -10,6 +10,21 @@ const DisplayAlbum = () => {
   return (
     <>
       <Navbar/>
+      <div className='mt-10 flex gap-8 flex-col md:flex-row mt:items-end'>
+        <img src ={albumData.image} alt='' className='w-48 rounded'/>
+            <div className='flex flex-col'> 
+                 <p>Playlist</p>
+                 <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
+                 <h4>{albumData.desc}</h4>
+                 <p className='mt-1'>
+                    <img className='w-5 inline-block mr-1' src={assets.spotify_logo}/>
+                    <b>Spotify</b>
+                    .1323154 likes
+                    .<b>50 songs</b>
+                     about 3 hr 37 min
+                 </p>
+            </div>
+        </div>
     </>
   )
 }
